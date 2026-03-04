629b3c3c582a14c893125d192bbb83f37045a355
Andrescg18 <chamolocoporsiempre@gmail.com>
2026-03-04 17:12:09 -0500

    Proyecto venta_motos

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,30 @@
-import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component } from '@angular/core';
+import { HeaderComponent } from './shared/header/header';
+import { CommonModule } from '@angular/common';
+
+interface Usuario {
+  nombre: string;
+  avatar: string;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 export class App {
-  protected readonly title = signal('tareas');
+  usuarios: Usuario[] = [
+    { nombre: 'Antonia Céspedes', avatar: 'https://i.pravatar.cc/100?img=1' },
+    { nombre: 'Emilia Torres', avatar: 'https://i.pravatar.cc/100?img=5' },
+    { nombre: 'Marcos Jeremías', avatar: 'https://i.pravatar.cc/100?img=3' },
+    { nombre: 'David Mercado', avatar: 'https://i.pravatar.cc/100?img=8' }
+  ];
+
+  usuarioSeleccionado?: Usuario;
+
+  seleccionarUsuario(user: Usuario) {
+    this.usuarioSeleccionado = user;
+  }
 }
